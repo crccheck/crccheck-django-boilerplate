a32646f30a5fa2496413c570c51413a6aa8ea724
crccheck <c@crccheck.com>
2013-03-26 07:54:37 +0000

    add default urls to go to the app

--- example_project/urls.py
+++ example_project/urls.py
@@ -6,9 +6,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'example_project.views.home', name='home'),
-    # url(r'^example_project/', include('example_project.foo.urls')),
+    url(r'^', include('{{ app_name }}.urls',
+        namespace='{{ app_name }}', app_name='{{ app_name }}')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
